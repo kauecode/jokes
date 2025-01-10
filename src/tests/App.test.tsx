69f1b90336c2efb766243e245bf0647ae1182858
Kaue <757664+kcorrea@users.noreply.github.com>
2025-01-10 17:55:12 -0500

    Finished setting up testing suite with React Testing Lib

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+// This test is just a test to test the testing :)
+// #TODO: Delete later
+describe('App Component', () => {
+  it('Renders basic elements on the app', () => {
+    render(<App />);
+    screen.debug();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toHaveTextContent(/vite/i);
+    expect(screen.getByAltText(/vite/i)).toBeInTheDocument();
+    expect(screen.getByAltText(/react/i)).toBeInTheDocument();
+  });
+});
