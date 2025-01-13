126bc95d3f0859564da0baf3b0017c9474010706
Kaue <757664+kcorrea@users.noreply.github.com>
2025-01-12 22:52:44 -0500

    Added App Component test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,15 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { vi } from 'vitest';
 import App from '../App';
 
-// This test is just a test to test the testing :)
-// #TODO: Delete later
+// Mock the API response
+vi.mock('../services/apiClient', () => {
+  return {
+    default: vi.fn().mockImplementation(() => ({
+      getJokes: vi.fn().mockResolvedValue({ jokes: [
+        { id: 1, setup: 'This is a joke setup', delivery: 'This is a joke delivery' }
+      ]})
+    }))
+  };
+});
+
 describe('App Component', () => {
-  it('Renders basic elements on the app', () => {
-    render(<App />);
-    screen.debug();
-    // expect(screen.getByRole('heading')).toBeInTheDocument();
-    // expect(screen.getByRole('heading')).toHaveTextContent(/vite/i);
-    // expect(screen.getByAltText(/vite/i)).toBeInTheDocument();
-    // expect(screen.getByAltText(/react/i)).toBeInTheDocument();
+  it('Should load a joke and display both parts on the screen', async () => {    
+    render(<App />);    
+    await waitFor(() => {
+      const setupText = screen.getByText(/This is a joke setup/i);
+      const deliveryText = screen.getByText(/This is a joke delivery/i);
+      expect(setupText).toBeInTheDocument();
+      expect(deliveryText).toBeInTheDocument();
+    }, { timeout: 2000 });  //Make sure we passed the loading screen since I have forced a delay for people to see the loaders.
   });
 });
--- src/tests/main.test.ts
+++
@@ -1,9 +0,0 @@
-import { it, expect, describe } from 'vitest'
-
-// This test is just a test to test the testing :)
-// #TODO: Delete later
-describe('group', () => {
-  it('should', () => {
-    expect(1).toBeTruthy()
-  })
-})
